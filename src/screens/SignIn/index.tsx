c146e080964ddd070c892ac9e3ca329edf41d269
Kumar Anurag <yoyoanusingh27@gmail.com>
2021-10-12 21:30:04 +0530

    fadeIn animation otp signin

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { View, Text, ActivityIndicator, Pressable } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { View, Text, ActivityIndicator, Pressable, Animated } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import styles from './styles';
 import { TextInput } from 'react-native-gesture-handler';
@@ -12,9 +12,8 @@ const SignIn = (props: any) => {
     const [mobile, setMobile] = useState("");
     const [mobileError, setMobileError] = useState("");
     const [isSubmitting, setIsSubmitting] = useState(false);
-    const [otp, setOtp] = useState(false);
 
-    const { setWhat } = useFirebaseContext();
+    const { } = useFirebaseContext();
 
     function onSubmit() {
         setIsSubmitting(true);
@@ -54,26 +53,47 @@ const SignIn = (props: any) => {
                                 </Pressable>
                             </View>
                         </> :
-                        <>
-                            <View style={styles.inpGroup}>
-                                <Text style={styles.labelText}>Enter OTP</Text>
-                                <TextInput style={styles.textInp} value={mobile} onChangeText={setMobile} keyboardType="phone-pad" defaultValue="xxxxxxxxxx" autoCompleteType="tel" />
-                            </View>
-                            <View style={styles.inpGroup}>
-                                <Pressable style={styles.submitBtn} onPress={()=> {
-                                    setOtp(true);
-                                    setTimeout(()=> {
-                                        setWhat("What");
-                                    }, 2000);
-                                }}>
-                                    {otp ? <ActivityIndicator size="large" color="#fff" /> : <Text style={styles.submitBtnText}>Verify</Text>}
-                                </Pressable>
-                            </View>
-                        </>
+                        <EnterOTP />
                     }
                 </View>
             </SafeAreaView>
         </View>
+    )
+}
+
+const EnterOTP = () => {
+    const [otpValue, setotpValue] = useState("")
+    const [otp, setOtp] = useState(false);
+    const fadeAnim = useRef(new Animated.Value(0)).current
+
+    const { setWhat } = useFirebaseContext();
+    useEffect(() => {
+        Animated.timing(
+            fadeAnim,
+            {
+                toValue: 1,
+                duration: 1000,
+                useNativeDriver: true,
+            }
+        ).start();
+    }, [fadeAnim])
+    return (
+        <Animated.View style={{ opacity: fadeAnim }}>
+            <View style={styles.inpGroup}>
+                <Text style={styles.labelText}>Enter OTP</Text>
+                <TextInput style={styles.textInp} value={otpValue} onChangeText={setotpValue} keyboardType="phone-pad" defaultValue="xxxxxxxxxx" autoCompleteType="tel" />
+            </View>
+            <View style={styles.inpGroup}>
+                <Pressable style={styles.submitBtn} onPress={() => {
+                    setOtp(true);
+                    setTimeout(() => {
+                        setWhat("What");
+                    }, 2000);
+                }}>
+                    {otp ? <ActivityIndicator size="large" color="#fff" /> : <Text style={styles.submitBtnText}>Verify</Text>}
+                </Pressable>
+            </View>
+        </Animated.View>
     )
 }
 
